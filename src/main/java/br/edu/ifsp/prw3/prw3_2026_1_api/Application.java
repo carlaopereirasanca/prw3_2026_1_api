daf52b07743951f02903c4550a02f6ede4ab0ca3
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2026-03-19 13:36:28 -0300

    apos aula 2

--- src/main/java/br/edu/ifsp/prw3/prw3_2026_1_api/Application.java
+++ src/main/java/br/edu/ifsp/prw3/prw3_2026_1_api/Application.java
@@ -2,8 +2,10 @@ package br.edu.ifsp.prw3.prw3_2026_1_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/edu/ifsp/prw3/prw3_2026_1_api/medico/DadosListagemMedico.java
@@ -0,0 +1,19 @@
+package br.edu.ifsp.prw3.prw3_2026_1_api.medico;
+
+public record DadosListagemMedico(String nome, String email,
+                                  String crm, Especialidade especialidade) {
+
+    // Construtor que recebe um objeto Medico, pega só os dados necessários,
+    // para criar este objeto DadosListagemMedico.
+
+    // OBS: Você pode criar diversos construtores específicos,
+    //      porém eles TEM QUE chamar o construtor padrão (this).
+
+    public DadosListagemMedico(Medico medico) {
+
+        this( medico.getNome(), medico.getEmail(),
+                medico.getCrm(), medico.getEspecialidade() );
+    }
+
+}
+
